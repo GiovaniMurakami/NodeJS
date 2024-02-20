75408b8907f874578ad10c3adfa28119e4c2a0e4
Giovani Murakami <giovani.murakami@outlook.com>
2024-02-20 15:03:26 -0300

    Added - arquivos aula 8

---
+++ 8_SQL/1_connection/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mysql = require('mysql')
+
+const app = express()
+app.use(express.static('public'))
+const hbs = exphbs.create({
+    partialsDir: ['views/partials'],
+})
+
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res)=>{
+    res.render('home')
+})
+
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Murakam1@',
+    database: 'nodeMysql'
+})
+conn.connect(function(err){
+    if (err){
+        console.log(err)
+        return
+    }
+    app.listen(3000, function(){
+        console.log('App running')
+    })
+})
