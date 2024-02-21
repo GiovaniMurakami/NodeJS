2781040475d6d4c24941fd61bc462b2ca0020059
Giovani Murakami <giovani.murakami@outlook.com>
2024-02-21 11:53:39 -0300

    Added - Arquivos aula 8

---
+++ 8_SQL/2_inserindo_dados/index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mysql = require('mysql')
+
+const app = express()
+app.use(express.static('public'))
+const hbs = exphbs.create({
+    partialsDir: ['views/partials'],
+})
+
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
+
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res)=>{
+    res.render('home')
+})
+
+app.post('/books/insertbook', (req, res) => {
+    const title = req.body.title
+    const pagesqty = parseInt(req.body.pagesqty)
+    const query = `INSERT INTO books (title, pagesqty) VALUES ('${title}', '${pagesqty}')`
+    conn.query(query, function(err){
+        if (err){
+            console.log(err)
+        }
+        res.redirect('/')
+    })
+})
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodeMysql'
+})
+
+conn.connect(function(err){
+    if (err){
+        console.log(err)
+        return
+    }
+    app.listen(3000, function(){
+        console.log('App running')
+    })
+})
