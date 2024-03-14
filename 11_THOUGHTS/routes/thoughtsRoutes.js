b3bc57c5da52dbca6021df3c2df08eeda929fe6c
Giovani Murakami <giovani.murakami@outlook.com>
2024-03-14 09:42:55 -0300

    Added - arquivos aula 11

---
+++ 11_THOUGHTS/routes/thoughtsRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const ThoughtController = require("../controllers/ThoughtsController");
+
+router.get("/", ThoughtController.showThoughts);
+
+module.exports = router;
